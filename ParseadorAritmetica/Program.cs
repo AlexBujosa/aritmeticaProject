39e9a0600324694f5a633bc9c92bfaae97c2110b
AlexBujosa <bujosa2014@outlook.com>
2022-11-12 17:48:03 -0400

    first commit

---
+++ ParseadorAritmetica/Program.cs
@@ -0,0 +1,12 @@
+﻿using Antlr4.Runtime;
+using ParseadorAritmetica;
+
+string input = "5*6+9*3-1";
+Console.WriteLine($"input: {input}\n");
+ICharStream stream = CharStreams.fromString(input);
+aritmeticaLexer lexer = new aritmeticaLexer(stream);
+CommonTokenStream token = new CommonTokenStream(lexer);
+aritmeticaParser parser = new aritmeticaParser(token);
+aritmeticaParser.AritmeticaContext tree =parser.aritmetica();
+Aritmetica aritmetica = new Aritmetica();
+int result = aritmetica.Visit(tree);
